d1745f83d173a88407034f07c2f44e21010687b1
md minarul islam <57551661+Nirob360@users.noreply.github.com>
2021-07-13 14:18:13 +0600

    Prototype js function and class javascript

---
+++ prototype.js
@@ -0,0 +1,110 @@
+
+
+// prototype in function 
+
+// function Persone(name, age) {
+    
+//     this.name = name;
+//     this.age = age;    
+
+// }
+// Persone.prototype = {
+    
+//     eat() {
+//         console.log(`${this.name} is eating. Age: ${this.age}`);        
+//     },
+//     sleep() {
+//         console.log(`${this.name} is sleeping. Age: ${this.age}`);       
+//     },
+//     play() {
+//         console.log(`${this.name} is playing. Age: ${this.age}`);        
+//     },
+//     dance() {
+//         console.log(`${this.name} is dansing. Age: ${this.age}`);       
+//     },
+//     watch() {
+//         console.log(`${this.name} is Watching. Age: ${this.age}`);        
+//     }
+
+// }
+
+// const Riya = new Persone("Riya", 17);
+// Riya.dance();
+
+// const Minarul = new Persone("Minarul", 23);
+// Minarul.watch();
+
+// const Rakib = new Persone("Rakib", 8);
+
+// Rakib.sleep();
+
+
+// prototype in class 
+
+
+
+
+// class Persone {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+
+//     };
+    
+
+//     eat() {
+//         console.log(`${this.name} is eating. Age: ${this.age}`);        
+//     }
+//     sleep() {
+//         console.log(`${this.name} is sleeping. Age: ${this.age}`);       
+//     }
+//     play() {
+//         console.log(`${this.name} is playing. Age: ${this.age}`);        
+//     }
+//     dance() {
+//         console.log(`${this.name} is dansing. Age: ${this.age}`);       
+//     }
+//     watch() {
+//         console.log(`${this.name} is Watching. Age: ${this.age}`);        
+//     } 
+
+// }
+
+
+// const Riya = new Persone("Riya", 17);
+// Riya.dance();
+
+// const Minarul = new Persone("Minarul", 23);
+// Minarul.watch();
+
+// const Rakib = new Persone("Rakib", 8);
+
+// Rakib.sleep(); 
+
+
+
+
+let persone = [];
+
+
+let persones = new Array();
+
+persones.push("Riya");
+
+console.log(Array.prototype);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
